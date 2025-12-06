4dbb475b41c2f5cdd2e06fef053fe967f92057a9
Michael Rothkopf <michaelhrothkopf@gmail.com>
2025-12-06 15:13:58 -0500

    updated site information

--- main.py
+++ main.py
@@ -19,13 +19,16 @@ add_website_css(SITE_STYLES)
 set_website_title("UD StudyPath")
 set_website_style("none")
 set_site_information(
-    "author",
-    """
-Your description can go here.
+    author="Max Polonlsky and Michael Rothkopf",
+    description="""UD StudyPath solves the limitations of Blue Hen Planner, WebReg, and Stellic by considering all aspects of course selection at once.
+
+    It takes into account the courses you should be taking to graduate on time, prerequisites, scheduling, and your personal preferences, allowing you to fully customize all aspects of your academic career at once.
+
+    Begin your UD StudyPath and UD study paths today!
 """,
-    [],
-    [],
-    [],
+    sources=["Used generative AI for some code generation", "https://gemini.google.com"],
+    planning=["https://docs.google.com/presentation/d/18-BhAJuDB6_1G6gx9oXqxJJQq_YlVO1RFA_q8HSKy40/edit?usp=sharing"],
+    links=["https://github.com/UD-F25-CS1/honors-hackathon-2025-team-max-and-michael"],
 )
 
 
